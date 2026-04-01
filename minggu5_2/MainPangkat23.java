9136d88d06865de9bc9b3215b3954d94dac0d8a9
ridoq <ridoq444@gmail.com>
2026-04-01 10:28:59 +0700

    Praktikum 5.3

---
+++ minggu5_2/MainPangkat23.java
@@ -0,0 +1,32 @@
+package minggu5_2;
+
+import java.util.Scanner;
+
+public class MainPangkat23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = input.nextInt();
+
+        Pangkat23[] png = new Pangkat23[elemen];
+
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan nilai basis elemen ke-" + (i + 1) + ": ");
+            int basis = input.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-" + (i + 1) + ": ");
+            int pangkat = input.nextInt();
+            png[i] = new Pangkat23(basis, pangkat);
+        }
+
+        System.out.println("HASIL PANGKAT BRUTEFORCE:");
+        for (Pangkat23 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatBF(p.nilai, p.pangkat));
+        }
+
+        System.out.println("HASIL PANGKAT DIVIDE AND CONQUER:");
+        for (Pangkat23 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatDC(p.nilai, p.pangkat));
+        }
+        input.close();
+    }
+}
