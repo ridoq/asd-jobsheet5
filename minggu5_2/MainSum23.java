ef26f1c88ce823a4b6a525364602b709c8b7c658
ridoq <ridoq444@gmail.com>
2026-04-01 10:38:14 +0700

    Praktikum 5.4

---
+++ minggu5_2/MainSum23.java
@@ -0,0 +1,21 @@
+package minggu5_2;
+
+import java.util.Scanner;
+
+public class MainSum23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = input.nextInt();
+
+        Sum23 sm = new Sum23(elemen);
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan keuntungan ke-" + (i + 1) + ": ");
+            sm.keuntungan[i] = input.nextDouble();
+        }
+
+        System.out.println("Total keuntungan menggunakan Bruteforce: " + sm.totalBF());
+        System.out.println("Total keuntungan menggunakan Divide and Conquer: " + sm.totalDC(sm.keuntungan, 0, elemen - 1));
+        input.close();
+    }
+}
---
+++ minggu5_2/Sum23.java
@@ -0,0 +1,30 @@
+package minggu5_2;
+
+public class Sum23 {
+    public int elemen;
+    public double keuntungan[];
+
+    Sum23(int el) {
+        this.elemen = el;
+        this.keuntungan = new double[el];
+    }
+
+    double totalBF() {
+        double total = 0;
+        for (int i = 0; i < keuntungan.length; i++) {
+            total = total + keuntungan[i];
+        }
+        return total;
+    }
+    
+    double totalDC(double arr[], int l, int r) {
+        if (l == r) {
+            return arr[l];
+        } else {
+            int mid = (l + r) / 2;
+            double lsum = totalDC(arr, l, mid);
+            double rsum = totalDC(arr, mid + 1, r);
+            return lsum + rsum; 
+        }
+    }
+}
